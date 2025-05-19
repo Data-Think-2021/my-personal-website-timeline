f6b00494e553b0eaa6664cfb86d3471c618bbe4a
Data-Think-2021 <xia.he88@gmail.com>
2025-05-19 22:40:19 +0200

    fix: the display

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
   images: { unoptimized: true },
+  basePath: '/personal-website-timeline', 
+  assetPrefix: '/personal-website-timeline/',
 };
 
 export default nextConfig;
