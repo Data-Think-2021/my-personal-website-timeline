4832d8cc620f40048b3456756ee99f4d447918fd
Data-Think-2021 <xia.he88@gmail.com>
2026-02-08 17:40:36 +0100

    feat: add book-call english url

---
+++ src/app/en/book-call/page.tsx
@@ -0,0 +1,11 @@
+import Navigation from '@/components/Navigation';
+import BookCall from '@/components/BookCall';
+
+export default function EnBookCallPage() {
+  return (
+    <>
+      <Navigation />
+      <BookCall />
+    </>
+  );
+}
